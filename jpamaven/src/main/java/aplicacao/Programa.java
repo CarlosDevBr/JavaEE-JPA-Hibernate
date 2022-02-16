bab85cdd6a5dbb186c483b8533c0fbe5dc26c159
cahzerohenrique@gmail.com <CarlosDevBr>
2022-02-15 22:35:49 -0300

    Gravando os dados no banco com o jpa

--- jpamaven/src/main/java/aplicacao/Programa.java
+++ jpamaven/src/main/java/aplicacao/Programa.java
@@ -1,5 +1,9 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
@@ -10,9 +14,14 @@ public class Programa {
 		Pessoa p2 = new Pessoa(1, "Diogo nogueira", "diogo@gmail.com");
 		Pessoa p3 = new Pessoa(1, "Maria da silva", "maria@gmail.com");
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();	
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+		System.out.println("Pronto");
 	}
 
 }
--- jpamaven/src/main/java/dominio/Pessoa.java
+++ jpamaven/src/main/java/dominio/Pessoa.java
@@ -2,12 +2,25 @@ package dominio;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Persistence;
+
+@Entity
 public class Pessoa implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String main;
 	private String email;
+	
+	
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 	
 	public Pessoa() {
 		
